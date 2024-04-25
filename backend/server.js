904ed53b33dda3d0a365afb11fdcc8a235b274ae
Siddharth Sharma <112341654+sid07003@users.noreply.github.com>
2024-04-25 21:17:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ app.use(cors({
     credentials:true
 }));
 
+app.use((req, res, next) => {
+    // Allow credentials only for requests coming from the specified origin
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.use(cookieParser());
 
 const { MongoClient, ObjectId } = require('mongodb');
